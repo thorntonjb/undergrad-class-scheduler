6031fad1d3de44f5156f499395b3d4bffe8591cd
john thornton <jb087011@gmail.com>
2024-04-29 11:05:34 -0400

    Create initial files
    Create files needed to complete the project and test the compiler for C Sharp files

---
+++ Access.cs
@@ -0,0 +1,4 @@
+public class Access : UndergradView, ClassView
+{
+
+}
---
+++ UndergradView.cs
@@ -0,0 +1,9 @@
+public class UndergradView
+{
+    private boolean clickedClassName;
+
+    public UndergradView(boolean _clickedClassName)
+    {
+        clickedClassName = _clickedClassName;
+    }
+}
